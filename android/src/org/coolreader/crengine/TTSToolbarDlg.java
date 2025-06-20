83f576237ca4fd4fa239d887815896769dc6254f
Elliot Wolk <elliot.wolk@gmail.com>
2025-06-20 12:29:10 -0400

    audiobook: rename sentenceinfo-cache-file => sentenceinfo-file

--- android/src/org/coolreader/crengine/TTSToolbarDlg.java
+++ android/src/org/coolreader/crengine/TTSToolbarDlg.java
@@ -105,7 +105,7 @@ public class TTSToolbarDlg implements Settings {
 	private int mTTSSpeedPercent = 50;		// 50% (normal)
 
 	private File wordTimingFile;
-	private File sentenceInfoCacheFile;
+	private File sentenceInfoFile;
 	private WordTimingAudiobookMatcher wordTimingAudiobookMatcher;
 	private SentenceInfo currentSentenceInfo;
 
@@ -732,7 +732,7 @@ public class TTSToolbarDlg implements Settings {
 		// Fetch book's metadata
 		BookInfo bookInfo = mReaderView.getBookInfo();
 		wordTimingFile = null;
-		sentenceInfoCacheFile = null;
+		sentenceInfoFile = null;
 		if (null != bookInfo) {
 			FileInfo fileInfo = bookInfo.getFileInfo();
 			if (null != fileInfo) {
@@ -747,7 +747,7 @@ public class TTSToolbarDlg implements Settings {
 				String sentenceInfoPath = pathName.replaceAll("\\.\\w+$", ".sentenceinfo");
 				if(wordTimingPath.matches(".*\\.wordtiming$")){
 					wordTimingFile = new File(wordTimingPath);
-					sentenceInfoCacheFile = new File(sentenceInfoPath);
+					sentenceInfoFile = new File(sentenceInfoPath);
 				}
 			}
 		}
@@ -802,10 +802,10 @@ public class TTSToolbarDlg implements Settings {
 			wordTimingCalcHandler.post(
 				new Runnable() {
 						public void run() {
-							List<SentenceInfo> allSentences = SentenceInfoCache.maybeReadCache(sentenceInfoCacheFile);
+							List<SentenceInfo> allSentences = SentenceInfoCache.maybeReadCache(sentenceInfoFile);
 							if(allSentences == null){
 								allSentences = mReaderView.getAllSentences();
-								SentenceInfoCache.maybeWriteCache(sentenceInfoCacheFile, allSentences);
+								SentenceInfoCache.maybeWriteCache(sentenceInfoFile, allSentences);
 							}
 							wordTimingAudiobookMatcher = new WordTimingAudiobookMatcher(wordTimingFile, allSentences);
 
